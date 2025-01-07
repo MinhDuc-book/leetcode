1a8c07dcdbee11269f8fe4fbdb1ae68116352dc1
Minh Đức <mduc7325@gmail.com>
2025-01-07 20:03:04 +0700

    Update FindTheOriginalString.cpp

--- FindTheOriginalString.cpp
+++ FindTheOriginalString.cpp
@@ -11,11 +11,14 @@ public:
         }
         // đếm số lần xuất hiện của từng kí tự (không tính lần đầu)
         int total = 0;
+
+        //đánh dấu các số lặp lại để không bị tính lại lần sau
         for (int i = 0; i < size; i = i+1) {
             if (word[i] == word[i+1]) {
                 check[i+1] = -10;
             }
         }
+        //tính tổng số lần lặp lại của các số có trong dãy và lưu vào biến arr
         for (int i = 0; i < size; i = i+1) {
             int count = 0;
             for (int j = 0; j < size; j = j+1) {
@@ -25,13 +28,14 @@ public:
             }
             arr[i] = count-1;
         }
-        // tổng số lần xuất hiện lặp lại của các kí tự
+        
+        // tổng của số lần xuất hiện lặp lại của các kí tự
         for (int i = 0; i < size; i = i+1) {
             if (check[i] != -10) {
                 total = total + arr[i];
             }
         }
-        
+        // cộng 1 bởi vì tính cả chuỗi gốc 
         return total+1;
     }
 };
