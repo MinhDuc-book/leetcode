cb9fb6e3741f93810d7b3747ad2bf8bab38af573
Minh Đức <mduc7325@gmail.com>
2025-05-01 23:02:10 +0700

    Medium

--- FindTheIndexOfTheFirstOccur.cpp
+++
@@ -1,9 +0,0 @@
-class Solution {
-public:
-    int strStr(string haystack, string needle) {
-        int index = 0;
-        // trả về giá trị đầu tiên mà needle xuất hiện trong haystack bằng lệnh find()
-        index = haystack.find(needle); 
-        return index;
-    }
-};
