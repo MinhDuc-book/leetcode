8ee4eadc5109d1d2a4c9b684948c0cb0c7be4400
Minh Đức <mduc7325@gmail.com>
2025-05-10 17:59:35 +0700

    Create isPowerOfThree.cpp

---
+++ isPowerOfThree.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    bool isPowerOfThree(int n) {
+        if (n == 3 or n == 1) {
+            return true;
+        }
+
+        if (n <= 0) {
+            return false;
+        }
+
+        return n % 3 == 0 and isPowerOfThree(n / 3);
+    }
+};
