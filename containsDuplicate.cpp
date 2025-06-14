1d97a787dcab47751059257b85cd584ece3ed93e
Minh Đức <mduc7325@gmail.com>
2025-06-14 22:22:49 +0700

    Update containsDuplicate.cpp

--- containsDuplicate.cpp
+++ containsDuplicate.cpp
@@ -13,3 +13,7 @@ public:
         return false;
     }
 };
+
+// Có cách khác là sử dụng hàm sort (nums.begin(), nums.end()) để sắp xếp
+// Sau khi sắp xếp thì các phần tử bị lặp lại sẽ ở gần nhau
+// Dùng lặp để duyệt qua và trả về kết quả
