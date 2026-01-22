dc207a34ef3647ddf15f3d4a2e9487b3c3aa404d
Minh Đức <mduc7325@gmail.com>
2026-01-22 23:24:16 +0700

    Create minDep.cpp

---
+++ minDep.cpp
@@ -0,0 +1,41 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int minDepth(TreeNode* root) {
+        int minDep = 0;
+
+        if (root == NULL) {
+            return 0;
+        }
+
+        if (root -> left == NULL and root -> right == NULL) {
+            return 1;
+        }
+
+        int minLeft = minDepth(root -> left);
+        int minRight = minDepth(root -> right);
+
+
+        if (minLeft + 1 == 1) {
+            minDep = minRight;
+        } else if (minRight + 1 == 1) {
+            minDep = minLeft;
+        } else {
+            minDep = min(minLeft, minRight);
+        }
+
+        return minDep + 1;
+
+    }
+
+};
